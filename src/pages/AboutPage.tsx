8ad5aa3de1c6f91b96670d4c9e1c6e4dd13f2ab0
Teameranet <pexonic.com@gmail.com>
2026-06-10 20:44:06 +0530

    Commit

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -51,7 +51,7 @@ const skillCategories: SkillCategory[] = [
   },
   {
     title: "Development",
-    skills: ["React.js", "Tailwind CSS", "JavaScript", "HTML", "GitHub"]
+    skills: ["React.js", "JavaScript", "Tailwind CSS", "HTML", "GitHub"]
   }
 ];
 
